22e256ffefc563d02435b8241d68c993e132c12b
Rob Hern <rob@thegoodideasgroup.com>
2020-02-13 10:48:09 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
 $(function() {
   console.log("ready!");
-  $("#myImg").hover(
-    function() {
-      $(this).attr("src", "animatedImg.gif");
-    },
-    function() {
-      $(this).attr("src", "staticImg.jpg");
-    }
-  );
 });
